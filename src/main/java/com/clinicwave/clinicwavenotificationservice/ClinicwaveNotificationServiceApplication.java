d3f69ba62590fe2bc5ba6b5e95bcd2f7b02c4506
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-07-08 00:14:41 +0530

    Initial commit

---
+++ src/main/java/com/clinicwave/clinicwavenotificationservice/ClinicwaveNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clinicwave.clinicwavenotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicwaveNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicwaveNotificationServiceApplication.class, args);
+	}
+
+}
