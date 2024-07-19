6afb95097be35b088d5153091835490b1b59fb03
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-07-20 00:02:04 +0530

    Add NotificationService Interface and Implementation for Flexible Notification Handling
    This commit introduces the following changes to the codebase:
    
    - Added `NotificationService` interface in the `com.clinicwave.clinicwavenotificationservice.service` package. This interface defines a contract for sending notifications, ensuring a consistent approach across different types of notifications.
    - Added `NotificationServiceImpl` class in the `com.clinicwave.clinicwavenotificationservice.service.impl` package. This class implements the `NotificationService` interface, providing the logic to send notifications using a strategy pattern. It supports different notification types through the use of `NotificationStrategy` implementations.
    - Introduced an `EnumMap` in `NotificationServiceImpl` to map `NotificationTypeEnum` to corresponding `NotificationStrategy` instances. This map facilitates the selection of the appropriate strategy for sending notifications based on the notification type.
    - Implemented dependency injection in `NotificationServiceImpl` to automatically inject a list of `NotificationStrategy` beans. This design allows for easy addition of new notification strategies without modifying the service class.
    - Added exception handling in `NotificationServiceImpl` to throw an `InvalidNotificationTypeException` when an unsupported notification type is requested. This ensures that the service responds gracefully to invalid notification type requests.

---
+++ src/main/java/com/clinicwave/clinicwavenotificationservice/service/impl/NotificationServiceImpl.java
@@ -0,0 +1,55 @@
+package com.clinicwave.clinicwavenotificationservice.service.impl;
+
+import com.clinicwave.clinicwavenotificationservice.dto.NotificationRequestDto;
+import com.clinicwave.clinicwavenotificationservice.enums.NotificationTypeEnum;
+import com.clinicwave.clinicwavenotificationservice.exception.InvalidNotificationTypeException;
+import com.clinicwave.clinicwavenotificationservice.service.NotificationService;
+import com.clinicwave.clinicwavenotificationservice.strategy.NotificationStrategy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * This class is a service class for the Notification entity.
+ * It provides methods to send email notifications.
+ *
+ * @author aamir on 7/8/24
+ */
+@Service
+@Slf4j
+public class NotificationServiceImpl implements NotificationService {
+  private final Map<NotificationTypeEnum, NotificationStrategy> notificationStrategyMap;
+
+  /**
+   * Constructor for dependency injection.
+   *
+   * @param notificationStrategyList The list of notification strategies to be used for sending notifications.
+   */
+  @Autowired
+  public NotificationServiceImpl(List<NotificationStrategy> notificationStrategyList) {
+    // Initialize the notification strategy map with an EnumMap
+    notificationStrategyMap = new EnumMap<>(NotificationTypeEnum.class);
+
+    // Add each notification strategy to the map using its type as the key
+    notificationStrategyList.forEach(strategy -> notificationStrategyMap.put(strategy.getType(), strategy));
+    log.info("Notification strategies initialized: {}", notificationStrategyMap.keySet());
+  }
+
+  /**
+   * Sends a notification using the appropriate strategy based on the notification type.
+   *
+   * @param notificationRequestDto The notification request to be sent.
+   * @throws InvalidNotificationTypeException if the notification type is not supported.
+   */
+  @Override
+  public void sendNotification(NotificationRequestDto notificationRequestDto) {
+    Optional.ofNullable(notificationStrategyMap.get(notificationRequestDto.type()))
+            .orElseThrow(() -> new InvalidNotificationTypeException("NotificationStrategy", "type", notificationRequestDto.type()))
+            .send(notificationRequestDto);
+  }
+}
